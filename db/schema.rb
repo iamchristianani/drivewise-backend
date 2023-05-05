19f5caad3132852803cd3ceddbc327d5c6398ade
Yuvenal Njoroge <yuvenal.njoroge@gmail.com>
2023-05-06 01:40:16 +0300

    Create Reservation model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_05_223157) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_05_223739) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -39,10 +39,23 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_05_223157) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "reservations", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "car_id", null: false
+    t.date "reservation_date"
+    t.string "city"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["car_id"], name: "index_reservations_on_car_id"
+    t.index ["user_id"], name: "index_reservations_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "reservations", "cars"
+  add_foreign_key "reservations", "users"
 end
