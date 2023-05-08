bf71539c96de81fa9d85433e5c3a015e903192b7
Roy Jumah <royjumah17@gmail.com>
2023-05-08 19:01:45 +0300

    create index, create method in crontoller and fix turbo validation in model

--- app/controllers/api/v1/cars_controller.rb
+++ app/controllers/api/v1/cars_controller.rb
@@ -1,6 +1,29 @@
 class Api::V1::CarsController < ApplicationController
+ 
+
   def index
     @cars = Car.all
     render json: @cars
   end
+
+
+  def create 
+    @car = Car.new(car_params)
+    if @car.save
+      render json: @car, status: :created
+    else
+      render json: { error: 'Unable to create car.' }, status: 400
+    end
+  end
+  
+
+
+
+
+ private
+
+ def car_params
+  params.require(:car).permit(:make, :model, :year, :color, :body_type, :engine_capacity, :engine_cylinders, :turbo, :horsepower, :torque, :weight, :drivetrain, :transmission, :fuel_type, :doors, :seats, :mileage, :price, :description, :image)
+ end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :cars, only: [:index, :show, :create, :update, :destroy]
+      resources :cars, only: [:index, :show, :create, :destroy]
       resources :reservations, only: [:index, :show, :create, :update, :destroy]
       resources :users, only: [:index, :show, :create, :update, :destroy]
     end
