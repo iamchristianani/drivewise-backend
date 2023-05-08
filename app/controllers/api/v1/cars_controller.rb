5ad5e509c674ce5f6b008bc51bfdb02e43fb43c6
Roy Jumah <royjumah17@gmail.com>
2023-05-08 19:11:26 +0300

    fix linting errors

--- app/controllers/api/v1/cars_controller.rb
+++ app/controllers/api/v1/cars_controller.rb
@@ -1,13 +1,10 @@
 class Api::V1::CarsController < ApplicationController
- 
-
   def index
     @cars = Car.all
     render json: @cars
   end
 
-
-  def create 
+  def create
     @car = Car.new(car_params)
     if @car.save
       render json: @car, status: :created
@@ -15,29 +12,26 @@ class Api::V1::CarsController < ApplicationController
       render json: { error: 'Unable to create car.' }, status: 400
     end
   end
-  
 
-
- def show
-  @car = Car.find(params[:id])
-  render json: @car
- end
-
-
- def destroy 
-  @car = Car.find(params[:id])
-  if @car
-    @car.destroy
-    render json: {message: 'Car deleted successfully.'}, status: 200
-  else
-    render error: {error: 'Unable to delete car.'}, status: 400
+  def show
+    @car = Car.find(params[:id])
+    render json: @car
   end
- end
 
- private
+  def destroy
+    @car = Car.find(params[:id])
+    if @car
+      @car.destroy
+      render json: { message: 'Car deleted successfully.' }, status: 200
+    else
+      render error: { error: 'Unable to delete car.' }, status: 400
+    end
+  end
 
- def car_params
-  params.require(:car).permit(:make, :model, :year, :color, :body_type, :engine_capacity, :engine_cylinders, :turbo, :horsepower, :torque, :weight, :drivetrain, :transmission, :fuel_type, :doors, :seats, :mileage, :price, :description, :image)
- end
+  private
 
+  def car_params
+    params.require(:car).permit(:make, :model, :year, :color, :body_type, :engine_capacity, :engine_cylinders, :turbo,
+                                :horsepower, :torque, :weight, :drivetrain, :transmission, :fuel_type, :doors, :seats, :mileage, :price, :description, :image)
+  end
 end
