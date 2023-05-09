724fac920ecee6cfa03614c69ac0cfa8b0c921c6
Roy Jumah <royjumah17@gmail.com>
2023-05-09 11:26:13 +0300

    add destroy and update methods to users controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -18,6 +18,26 @@ class Api::V1::UsersController < ApplicationController
     render json: @user
   end
 
+  def destroy
+    @user = User.find(params[:id])
+    if @user
+      @user.destroy
+      render json: { message: 'user deleted successfully.' }, status: 200
+    else
+      render error: { error: 'Unable to delete user.' }, status: 400
+    end
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user
+      @user.update(user_params)
+      render json: { message: 'user successfully updated.' }, status: 200
+    else
+      render error: { error: 'Unable to update user.' }, status: 400
+    end
+  end
+
   private
 
   def user_params
