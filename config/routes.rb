b7132b4dd65d5e451bdc583e6a8ebe4e52140c47
Yuvenal Njoroge <yuvenal.njoroge@gmail.com>
2023-05-09 15:30:53 +0300

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      resources :users, only: %i[index show create update destroy] do
+        resources :reservations, only: %i[index show create update destroy] do
+          resources :cars, only: %i[index show create destroy]
+        end
+      end
       resources :cars, only: %i[index show create destroy]
-      resources :reservations, only: %i[index show create update destroy]
-      resources :users, only: %i[index show create update destroy]
+      resources :reservations, only: %i[index show]
     end
   end
 end
