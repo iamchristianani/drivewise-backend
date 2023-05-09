5f6df28c6282d7fccf2415b2691a4388d33b0a8a
Roy Jumah <royjumah17@gmail.com>
2023-05-09 11:24:59 +0300

    Add create, show and index method to users controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,4 +3,25 @@ class Api::V1::UsersController < ApplicationController
     @users = User.all
     render json: @users
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      render json: @user, status: :created
+    else
+      render json: { error: 'Unable to create user.' }, status: 400
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+    render json: @user
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username)
+  end
+
 end
