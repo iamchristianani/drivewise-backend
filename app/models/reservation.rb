bb6fa4578c89cab2c6400087ef3f6315a6f064af
Christian Ani <iamchristianani@gmail.com>
2023-05-18 23:02:15 +0100

    Fix reservation date

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,11 +1,19 @@
-require 'timeliness'
-
 class Reservation < ApplicationRecord
   belongs_to :user
   belongs_to :car
 
-  validates :reservation_date, presence: true, timeliness: { type: :date }
+  validates :reservation_date, presence: true
+  validate :validate_reservation_date_format
+
   validates :city, presence: true
   validates :user_id, presence: true, numericality: { only_integer: true }
   validates :car_id, presence: true, numericality: { only_integer: true }
+
+  private
+
+  def validate_reservation_date_format
+    return if reservation_date.blank? || reservation_date.is_a?(Date)
+
+    errors.add(:reservation_date, 'must be a valid date')
+  end
 end
